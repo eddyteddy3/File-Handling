09b53db19dd76d3135bafc232a45716ab0dac492
Mouzzam <moazzamtahir81@gmail.com>
2019-09-02 01:21:12 +0500

    Created the function to add the custom file to document directory path

--- File Handling/ViewController.swift
+++ File Handling/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    var fileMng: FileManager = FileManager.default //selecting the defualt location of App Sandbox File.
+    var docsDir: String? //string to store the document directory from user's domain
+    var dataFile: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        checkFile()
     }
 
-
+    func checkFile() {
+        //user domain mask is user's app home directory
+        let dirPath = fileMng.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        dataFile = dirPath[0].appendingPathComponent("datafile.dat").path //getting the path as String
+        
+        if fileMng.fileExists(atPath: dataFile) { //check whether the file path exists
+            if let dataBuffer = fileMng.contents(atPath: dataFile) { //getting the contents of file
+                let dataString = NSString(data: dataBuffer, encoding: String.Encoding.utf8.rawValue)
+                textField.text = dataString as String?
+            }
+        }
+    }
+    
+    @IBAction func saveText(_ sender: Any) {
+    }
+    
 }
 
